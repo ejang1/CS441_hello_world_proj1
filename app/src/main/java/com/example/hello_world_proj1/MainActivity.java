c565bfcf01edb9ef0462fa37269a59705f1f3462
ejang1 <ejang1@binghamton.edu>
2021-08-30 17:01:54 -0400

    UI change, function added

--- app/src/main/java/com/example/hello_world_proj1/MainActivity.java
+++ app/src/main/java/com/example/hello_world_proj1/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.hello_world_proj1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
 import android.os.Bundle;
-import java.util.ArrayList;
+
+import java.util.Map;
+import java.util.Random;
+import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> colorset = new ArrayList<String>();
+    Map<Integer,String> colorset;
+    File colorinfo; //= new File("CS441_hello_world_proj1\\colorInfo\\colornames.txt");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +22,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void changeText(View view){
+    public void buttonPress(View view){
 //        if(view.getDrawingCacheBackgroundColor() == Color.BLUE) view.setBackgroundColor(Color.RED);
 //        else view.setBackgroundColor(Color.BLUE);
-        TextView textview = (TextView)findViewById(R.id.textView);
-        ConstraintLayout mainConstraintLayout = (ConstraintLayout)findViewById(R.id.main);
-        mainConstraintLayout.setBackgroundColor(0xFF40CE93);
+        Random random = new Random();
+        int randNum = random.nextInt(2628595);
+        changeBackgroundColor(randNum);
+        updateColorInfo(randNum);
+        updateColorName(randNum);
+
+        TextView textview = (TextView)findViewById(R.id.colorNameTag);
         textview.setText("new text");
     }
+    public void changeBackgroundColor(int index){
+        ConstraintLayout mainConstraintLayout = (ConstraintLayout)findViewById(R.id.main);
+        mainConstraintLayout.setBackgroundColor(0xFF40CE93);
+    }
+    public void updateColorInfo(int index){
+        TextView colorHexInfo = (TextView)findViewById(R.id.colorNameTag);
 
+    }
+    public void updateColorName(int index){}
 }
